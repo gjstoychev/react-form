c873c23382805ac8c919302e635a08772c8482c7
Gancho Stoychev <gancho.stoychev@mvpbg.com>
2021-12-13 17:38:37 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from 'react'
 
 function App() {
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    inputColor: '',
+    buttonColor: '',
+    buttonLabel: 'Submit'
+  })
+
+  const updateInput = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const changeBorderStyle = () => {
+    const inputColor = `#${getRandomInt(100000, 999999)}`;
+
+    setUser({
+      ...user,
+      inputColor
+    })
+  }
+
+  const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min
+
+  const onSubmit = () => {
+    setUser({
+      ...user,
+      buttonColor: '',
+      buttonLabel: 'Loading...'
+    })
+
+    const query = {
+      first_name: user.firstName,
+      last_name: user.lastName,
+      email: user.email
+    }
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({query})
+    }
+
+    fetch('https://reqres.in/api/posts', requestOptions) // fetch('https://localhost:4000/api/user', requestOptions)
+      .then(res => {
+        setUser({
+          ...user,
+          buttonColor: '#3CB371',
+          buttonLabel: 'Success'
+        })
+        return res
+      })
+      .catch(err => {
+        setUser({
+          ...user,
+          buttonColor: '#FA8072',
+          buttonLabel: 'Error'
+        })
+        return err
+      })
+    }
+
+  const inputStyle = {
+    border: `2px solid ${user.inputColor}`
+  }
+
+  const buttonStyle = {
+    backgroundColor: user.buttonColor
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div>
+        <input 
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          style={inputStyle}
+          onChange={updateInput}
+          onBlur={changeBorderStyle}
+        />
+      </div>
+
+      <div>
+        <input 
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          style={inputStyle}
+          onChange={updateInput}
+          onBlur={changeBorderStyle}
+        />
+      </div>
+
+      <div>
+        <input 
+          type="email"
+          name="email"
+          placeholder="Email"
+          className="input-item"
+          style={inputStyle}
+          onChange={updateInput}
+          onBlur={changeBorderStyle}
+        />
+      </div>
+
+      <button
+        style={buttonStyle}
+        onClick={onSubmit}
+      >
+        {user.buttonLabel}
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
